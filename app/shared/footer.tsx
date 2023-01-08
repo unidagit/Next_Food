71392cd1222ecede54ee6651b5f792a6db0b28f0
unidagit <rhcjrdbsgml@gmail.com>
2023-01-08 22:31:52 +0900

    배너 추가

--- app/shared/footer.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Footer() {
-  return <div>footer입니다</div>;
-}
-
-export default Footer;
---
+++ components/banner/Banner.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Banner.module.css";
+
+function Banner() {
+  return <div className={styles.container}>Banner</div>;
+}
+
+export default Banner;
