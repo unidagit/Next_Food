647f6b4b7cf4ce6e0ab8e4d43a7d57f50fbfb9cb
unidagit <rhcjrdbsgml@gmail.com>
2023-01-10 23:23:49 +0900

    text 컴포넌트 추가

---
+++ components/text/Text.tsx
@@ -0,0 +1,15 @@
+import clsx from "clsx";
+import styles from "./Text.module.css";
+
+function Text({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  //Text컴포넌트로 감싸진 내부 요소들이 children으로 들어온다
+  return <p className={clsx(styles.text, className)}>{children}</p>;
+}
+
+export default Text;
