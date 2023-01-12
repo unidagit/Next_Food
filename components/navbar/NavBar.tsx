776e39e6bbad10038068d3c48c8eac4b303d1faf
unidagit <rhcjrdbsgml@gmail.com>
2023-01-12 22:01:35 +0900

    navbar 수정

--- components/navbar/NavBar.tsx
+++ components/navbar/NavBar.tsx
@@ -1,27 +1,22 @@
 import Link from "next/link";
 import styles from "./NavBar.module.css";
+import { titleFont } from "../../utils/fonts";
 
 function Navbar() {
   return (
     <div className={styles.container}>
       <ul className={styles.wrapper}>
         <li className={styles.left}>
-          <Link href="/">BookHome</Link>
-        </li>
-
-        <li className={styles.center}>
-          <input
-            className={styles.searchInput}
-            placeholder={"책을 검색해보세요!"}
-          />
-          {/* <img imgSrc={search} width="28px" height="28px" /> */}
+          <Link href="/">
+            <p className={titleFont.className}>CookBook</p>
+          </Link>
         </li>
 
         <li className={styles.right}>
-          <Link className={styles.loginButton} href="/user">
-            로그인
+          <Link className={styles.foodButton} href="/food">
+            Food
           </Link>
-          <Link href="/carts">장바구니</Link>
+          <Link href="/savedMeals">찜</Link>
         </li>
       </ul>
     </div>
