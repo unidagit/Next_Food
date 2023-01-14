bc4b55232ce797426f2801327f40d8a1fa9201a6
unidagit <rhcjrdbsgml@gmail.com>
2023-01-14 14:11:31 +0900

    select props 전달

--- app/food/page.tsx
+++ app/food/page.tsx
@@ -1,25 +1,30 @@
 "use client";
 import { useQuery } from "@tanstack/react-query";
-import React from "react";
+import React, { useState } from "react";
 import Categories from "../../components/categories/Categories";
 import SearchFood from "../../components/searchFood/SearchFood";
 import { getCategories, ICategories } from "../../lib/api";
 
 function SearchFoodPage() {
+  const [select, setSelect] = useState("");
   //categories 리액트쿼리로 데이터 가져와서 버튼만들기
   const {
     data: categories,
     isLoading: categoriesLoading,
     isError: categoriesError,
+    error: categoriesErrorMessage,
   } = useQuery<ICategories[]>(["categories"], getCategories);
+  console.log(select);
 
   return (
     <div>
-      <SearchFood />
+      {/* <SearchFood /> */}
       <Categories
+        setSelect={setSelect}
         categories={categories}
         categoriesLoading={categoriesLoading}
         categoriesError={categoriesError}
+        categoriesErrorMessage={categoriesErrorMessage}
       />
     </div>
   );
