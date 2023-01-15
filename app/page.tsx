4ae68d2e0d95f3c0eebf45b58edeadd2d566433c
unidagit <rhcjrdbsgml@gmail.com>
2023-01-15 22:47:26 +0900

    컴포넌트 제거

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
-import Head from "next/head";
 import Banner from "../components/banner/Banner";
-import BestBookSection from "../components/sections/BestBookSection/BestBookSection";
-// import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
       <Banner />
-      <BestBookSection />
     </div>
   );
 }
