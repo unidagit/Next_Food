4ca2ce45d7b5e74d5ea262eeb9c5da83ac12db18
unidagit <rhcjrdbsgml@gmail.com>
2023-01-15 22:48:30 +0900

    font 설정 추가

---
+++ utils/fonts.ts
@@ -0,0 +1,11 @@
+import { Exo_2, Noto_Sans } from "@next/font/google";
+
+export const titleFont = Exo_2({
+  weight: ["500", "600", "700"],
+  subsets: ["latin"],
+});
+
+export const textFont = Noto_Sans({
+  weight: ["500", "600", "700"],
+  subsets: ["latin"],
+});
