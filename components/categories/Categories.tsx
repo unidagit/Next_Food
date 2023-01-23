52644e69d7478b8c821dfe98ede38843a8458dc2
unidagit <rhcjrdbsgml@gmail.com>
2023-01-23 18:13:06 +0900

    Design : style 변경

--- components/categories/Categories.tsx
+++ components/categories/Categories.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ICategories, ICategoriesProps } from "../../lib/api";
+import { ICategoriesProps } from "../../lib/api";
 import Loading from "../loading/Loading";
 import styles from "./Categories.module.css";
 import CategoryItem from "./CategoryItem";
@@ -19,7 +19,9 @@ function Categories({
     return <>Error:{categoriesErrorMessage}</>;
   }
 
-  const onClickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+  const onClickHandler = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
     setSelectCategory(e); //내가 클릭한 인자를 함수에 담아서 보냄
   };
 
