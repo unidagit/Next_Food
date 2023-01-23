6ed25fc409ee15c41cda02146ee5618d5efb8942
unidagit <rhcjrdbsgml@gmail.com>
2023-01-23 18:19:55 +0900

    deleate

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Banner from "../components/banner/Banner";
-
-export default function Home() {
-  return (
-    <div>
-      <Banner />
-    </div>
-  );
-}
--- app/shared/navbar.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Navbar() {
-  return <div>navbar입니다</div>;
-}
-
-export default Navbar;
