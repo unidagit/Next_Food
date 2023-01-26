a400bc35f1fbf46e72204d7716666340393f7539
unidagit <rhcjrdbsgml@gmail.com>
2023-01-26 17:11:12 +0900

    Feat : catrgory 보여주기 구현

--- app/meals/page.tsx
+++ app/meals/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useQuery } from "@tanstack/react-query";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import MealCard from "../../components/cards/MealCard";
 import Categories from "../../components/categories/Categories";
 import Loading from "../../components/loading/Loading";
@@ -35,9 +35,12 @@ function MealsPage() {
     getQueryMeals
   );
 
-  // if (categoriesLoading || categoriesQueryLoading) {
-  //   return <Loading />;
-  // } 함수라서
+  //접속했을때 첫번쨰 버튼 카테고리아이템 띄워주기
+  useEffect(() => {
+    if (categories) {
+      setSelectCategory(categories[0].strCategory);
+    }
+  }, [categories]);
 
   return (
     <div>
@@ -55,7 +58,9 @@ function MealsPage() {
       <div className={styles.itemContainer}>
         {categoriesQueryData &&
           categoriesQueryData.map((meal) => (
-            <MealCard key={meal.idMeal} mealInfo={meal} />
+            <div key={meal.idMeal} className={styles.box}>
+              <MealCard key={meal.idMeal} mealInfo={meal} />
+            </div>
           ))}
       </div>
     </div>
