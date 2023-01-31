de86eef477d30f0f40a0beb119984f8722a21f8a
unidagit <rhcjrdbsgml@gmail.com>
2023-01-31 23:30:27 +0900

    Feat : skeleton 추가

--- components/img/MealImage.tsx
+++ components/img/MealImage.tsx
@@ -6,9 +6,12 @@ function MealImage({ imageAddress }: { imageAddress: string }) {
   return (
     <Image
       src={imageAddress}
-      fill
+      width={200}
+      height={200}
       className={styles.customImage}
       alt="mealsImage"
+      placeholder="blur" // 추가
+      blurDataURL="data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAFklEQVR42mN8//HLfwYiAOOoQvoqBABbWyZJf74GZgAAAABJRU5ErkJggg==" //추가
     />
   );
 }
