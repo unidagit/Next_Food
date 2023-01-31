2345e2724f4ed94af93560f74ea5a1801e6115d7
unidagit <rhcjrdbsgml@gmail.com>
2023-01-31 20:44:48 +0900

    Chore : ThemeProvider 설정 추가

--- provider/Provider.tsx
+++ provider/Provider.tsx
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ThemeProvider } from "next-themes";
 import React from "react";
 
 type ProviderProps = {
@@ -16,10 +17,12 @@ function Provider({ children }: ProviderProps) {
     },
   });
   return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <ThemeProvider enableSystem defaultTheme="light">
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </ThemeProvider>
   );
 }
 
