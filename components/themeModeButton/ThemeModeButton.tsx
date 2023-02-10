bbffc8a22eddf2abd17eae5b3b757a94f0c4baac
unidagit <rhcjrdbsgml@gmail.com>
2023-02-10 22:16:45 +0900

    Fix : button bug 재수정

--- components/themeModeButton/ThemeModeButton.tsx
+++ components/themeModeButton/ThemeModeButton.tsx
@@ -2,21 +2,23 @@ import { useTheme } from "next-themes";
 import Image from "next/image";
 import Light from "../../images/light.svg";
 import Dark from "../../images/dark.svg";
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 
 function ThemeModeButton() {
   const { theme, setTheme } = useTheme();
   console.log(theme);
 
   const themeModeHandle = () => {
-    setTheme(theme === "light" ? "dark" : "light");
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
   };
 
   useEffect(() => {
-    if (window.localStorage.getItem("theme") === "dark") {
-      return setTheme("dark");
-    } else {
-      return setTheme("light");
+    if (!window.localStorage.getItem("theme")) {
+      setTheme("light");
     }
   }, [setTheme]);
 
