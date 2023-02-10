9c02ad1e2c757bb7c71d1087acede32403bef7e4
unidagit <rhcjrdbsgml@gmail.com>
2023-02-10 21:33:07 +0900

    Fix : localStorage 버그 수정

--- components/themeModeButton/ThemeModeButton.tsx
+++ components/themeModeButton/ThemeModeButton.tsx
@@ -2,19 +2,23 @@ import { useTheme } from "next-themes";
 import Image from "next/image";
 import Light from "../../images/light.svg";
 import Dark from "../../images/dark.svg";
+import React, { useEffect } from "react";
 
 function ThemeModeButton() {
   const { theme, setTheme } = useTheme();
   console.log(theme);
-  const localTheme = window.localStorage.getItem("theme");
 
   const themeModeHandle = () => {
-    if (localTheme === "light") {
-      setTheme("dark");
-    } else {
-      setTheme("light");
-    }
+    setTheme(theme === "light" ? "dark" : "light");
   };
+
+  useEffect(() => {
+    if (window.localStorage.getItem("theme") === "dark") {
+      return setTheme("dark");
+    } else {
+      return setTheme("light");
+    }
+  }, [setTheme]);
 
   return (
     <>
