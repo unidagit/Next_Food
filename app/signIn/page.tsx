a0e0a016c29f52604ae72c2497e59d4b904ec538
unidagit <rhcjrdbsgml@gmail.com>
2023-02-19 17:10:17 +0900

    Feature  :  SignInPage 컴포넌트 추가

---
+++ app/signIn/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SignInForm from "../../containers/signInForm/SignInForm";
+
+function SignInPage() {
+  return (
+    <div>
+      <SignInForm />
+    </div>
+  );
+}
+
+export default SignInPage;
