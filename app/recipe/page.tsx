411d2eeaf65e901e324e38d2c4af94c4f71b53ce
unidagit <rhcjrdbsgml@gmail.com>
2023-02-20 23:49:15 +0900

    Fix : "use client" 추가

--- app/recipe/page.tsx
+++ app/recipe/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Link from "next/link";
 import Wrapper from "../../components/common/wrapper/Wrapper";
 import RecipeListForm from "../../containers/recipeListForm/RecipeListForm";
