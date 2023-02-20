4f51dadb5ba3355b7ca6157260b081382afbd9d6
unidagit <rhcjrdbsgml@gmail.com>
2023-02-20 23:04:42 +0900

    Feature : RecipePage 추가

--- app/recipe/page.tsx
+++ app/recipe/page.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import Link from "next/link";
+import Wrapper from "../../components/common/wrapper/Wrapper";
+import RecipeListForm from "../../containers/recipeListForm/RecipeListForm";
 
 function RecipePage() {
-  return <div>RecipePage</div>;
+  return (
+    <>
+      <Wrapper>
+        <Link href="/recipe/write">레시피 글쓰기</Link>
+        <RecipeListForm />
+      </Wrapper>
+    </>
+  );
 }
 
 export default RecipePage;
