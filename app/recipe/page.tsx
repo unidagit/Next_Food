62b3a53bba0477fbf19dbff1854414ef4299a96b
unidagit <rhcjrdbsgml@gmail.com>
2023-02-27 22:59:52 +0900

    Feature : private router 생성

--- app/recipe/page.tsx
+++ app/recipe/page.tsx
@@ -1,9 +1,18 @@
 "use client";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import Wrapper from "../../components/common/wrapper/Wrapper";
 import RecipeListForm from "../../containers/recipeListForm/RecipeListForm";
 
 function RecipePage() {
+  const router = useRouter();
+  const token = localStorage.getItem("token_");
+
+  if (!token) {
+    alert("로그인을 해주세요.");
+    router.push("/signIn");
+  }
+
   return (
     <>
       <Wrapper>
