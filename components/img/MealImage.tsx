199665a658e7d65780891aaf41dd732d3bb62297
unidagit <rhcjrdbsgml@gmail.com>
2023-02-28 15:36:00 +0900

    Design : style수정

--- components/img/MealImage.tsx
+++ components/img/MealImage.tsx
@@ -4,14 +4,16 @@ import styles from "./MealImage.module.css";
 
 function MealImage({ imageAddress }: { imageAddress: string }) {
   return (
-    <Image
-      fill
-      src={imageAddress}
-      className={styles.customImage}
-      alt="mealsImage"
-      placeholder="blur" // 추가
-      blurDataURL="data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAFklEQVR42mN8//HLfwYiAOOoQvoqBABbWyZJf74GZgAAAABJRU5ErkJggg==" //추가
-    />
+    <div className={styles.container}>
+      <Image
+        fill
+        src={imageAddress}
+        className={styles.customImage}
+        alt="mealsImage"
+        placeholder="blur" // 추가
+        blurDataURL="data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAFklEQVR42mN8//HLfwYiAOOoQvoqBABbWyZJf74GZgAAAABJRU5ErkJggg==" //추가
+      />
+    </div>
   );
 }
 
