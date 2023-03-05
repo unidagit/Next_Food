ec91253afeba6721797f8f86eca99133f3950ebd
unidagit <rhcjrdbsgml@gmail.com>
2023-03-05 22:52:27 +0900

    Fix : RecipeDetailpage 수정

--- app/recipe/[recipeId]/page.tsx
+++ app/recipe/[recipeId]/page.tsx
@@ -1,7 +1,40 @@
+"use client";
+import { useSearchParams } from "next/navigation";
+// import Loading from "../../../components/loading/Loading";
 import RecipeIdForm from "../../../containers/recipeIdForm/RecipeIdForm";
+import useGetMyRecipeDetailQuery from "../../../hooks/queries/useGetMyRecipeDetailQuery";
 
 function RecipeDetailpage() {
-  return <RecipeIdForm />;
+  const recipeParams = useSearchParams();
+  const recipeId = recipeParams.get("r");
+  const {
+    data: myRecipeData,
+    // isLoading,
+    // isError,
+    // error,
+  } = useGetMyRecipeDetailQuery(recipeId);
+
+  // if (isLoading || !myRecipeData) {
+  //   return <Loading />;
+  // }
+
+  // if (isError) {
+  //   return console.log(error);
+  // }
+
+  return (
+    <>
+      {myRecipeData && recipeId && (
+        <RecipeIdForm
+          itemName={myRecipeData.itemName}
+          price={myRecipeData.price}
+          link={myRecipeData.link}
+          itemImage={myRecipeData.itemImage}
+          id={recipeId}
+        />
+      )}
+    </>
+  );
 }
 
 export default RecipeDetailpage;
