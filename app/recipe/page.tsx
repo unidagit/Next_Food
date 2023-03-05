0181cdb1adea02b5bc339899f5e6214562dd8cf2
unidagit <rhcjrdbsgml@gmail.com>
2023-03-05 23:04:37 +0900

    Feat : RecipePage UI 구현

--- app/recipe/page.tsx
+++ app/recipe/page.tsx
@@ -1,4 +1,29 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import Wrapper from "../../components/common/wrapper/Wrapper";
+import RecipeForm from "../../containers/recipeForm/RecipeForm";
+import RecipeListForm from "../../containers/recipeListForm/RecipeListForm";
+import getUserAccountname from "../../utils/getUserAccountname";
+
 function RecipePage() {
+  // const [account, setAccount] = useState("");
+  // const router = useRouter();
+  // const accountname = getUserAccountname();
+
+  // const [usesrData, setUserData] = useState("");
+
+  // const userAccountname =
+  //   typeof window !== "undefined" ? localStorage.getItem("accountname") : null;
+
+  // useEffect(() => {
+  //   if (userAccountname) {
+  //     setUserData(userAccountname);
+  //   } else {
+  //     alert("로그인해");
+  //   }
+  // }, []);
+
   return (
     <Wrapper>
       <RecipeForm />
@@ -8,8 +33,3 @@ function RecipePage() {
 }
 
 export default RecipePage;
-
-import React from "react";
-import Wrapper from "../../components/common/wrapper/Wrapper";
-import RecipeForm from "../../containers/recipeForm/RecipeForm";
-import RecipeListForm from "../../containers/recipeListForm/RecipeListForm";
