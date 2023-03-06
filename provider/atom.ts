8c377b8cfc61535bd80f13b644654f49c7d9e3d5
unidagit <rhcjrdbsgml@gmail.com>
2023-03-06 17:10:28 +0900

    Feature : recoil-persist 새로고침 후 데이터 유지 기능 추가

--- provider/atom.ts
+++ provider/atom.ts
@@ -1,6 +1,16 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const recoilLocalStorage =
+  typeof window !== "undefined" ? window.localStorage : undefined;
+
+const { persistAtom } = recoilPersist({
+  key: "recoil-persist",
+  storage: recoilLocalStorage,
+});
 
 export const isUserAtom = atom({
   key: "userToken",
   default: "",
+  effects_UNSTABLE: [persistAtom],
 });
