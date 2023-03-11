5f82edf79dde1627e01f967c4e843179ca4c9fb5
unidagit <rhcjrdbsgml@gmail.com>
2023-03-11 14:27:32 +0900

    Feature : enabled 설정 추가

--- hooks/queries/useGetMyRecipeQuery.tsx
+++ hooks/queries/useGetMyRecipeQuery.tsx
@@ -3,8 +3,12 @@ import { REACT_QUERY_KEY } from "../../common/constants";
 import { getRecipeList, IrecipeListData } from "../../lib/api";
 
 function useGetMyRecipeQuery(accountname: string | null) {
-  return useQuery<IrecipeListData>([REACT_QUERY_KEY.GET_RECIPE], () =>
-    getRecipeList(accountname)
+  return useQuery<IrecipeListData>(
+    [REACT_QUERY_KEY.GET_RECIPE],
+    () => getRecipeList(accountname),
+    {
+      enabled: !!accountname,
+    }
   );
 }
 
